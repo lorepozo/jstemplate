e9a835ba68807bb5a6b84f44f79b56216b2a0543
Lucas Morales <lucasem@Lucass-MacBook-Pro.local>
2014-08-26 03:33:57 -0400

    Removed a space...

--- jstemplate.js
+++ jstemplate.js
@@ -2,9 +2,9 @@
 Element.prototype.moustache = function(o){
 	return this.innerHTML = new Function('o','var t=[];with(o){t.push("' +
 		this.innerHTML
-			.replace(/\s+/g, ' ')
+			.replace(/\s+/g,' ')
 			.replace(/"/,'\\"')
-			.replace(/{{[^\!](.*?)}}/g, '",$1,"')
+			.replace(/{{[^\!](.*?)}}/g,'",$1,"')
 			.split("{{!").join('");')
 			.split("}}").join('t.push("')
 			+ '")}return t.join("");')(o||{});
